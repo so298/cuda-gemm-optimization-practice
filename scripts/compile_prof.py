6a8c0e204e13fe9b334d774e2895bd2bc81b7f07
Sosuke Hosokawa <47357860+so298@users.noreply.github.com>
2024-04-23 18:24:58 +0900

    Modify scripts

--- scripts/compile_prof.py
+++ scripts/compile_prof.py
@@ -12,14 +12,19 @@ def main():
     print("inputs:", files)
     print("output:", output)
     
-    df_out = pd.read_csv(files[0])[['Section Name', 'Metric Name', 'Metric Unit']]
+    df_out = pd.read_csv(files[0], index_col="Metric Name")[['Section Name', 'Metric Unit']]
+    rows = []
     for file in files:
-        df = pd.read_csv(file)
+        df = pd.read_csv(file, index_col="Metric Name")
 
         # Change the column name to the file name
         file_name = file.split('/')[-1].split('.')[0]
+        rows.append(file_name)
         df[file_name] = df['Metric Value']
         df_out = pd.merge(df_out, df[[file_name]], left_index=True, right_index=True)
+    
+    df_out.reset_index(inplace=True)
+    df_out.reindex(columns=['Section Name', 'Metric Name', 'Metric Unit'] + rows)
     
     df_out.to_csv(output, index=False)
 
